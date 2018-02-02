a517a9bb9b41652771a086bafdb124dfafa20a72
Training_C2a.05.01 <Training_C2a.05.01@accenture.com>
2018-02-02 16:16:18 +0530

    auto

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 package org.springframework.samples.petclinic.model;
-
+//adfsaf
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
